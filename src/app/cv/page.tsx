2e20e21f5c693d73d2bb8e5307619969464bddeb
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-16 16:36:09 +0530

    Update page.tsx

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -14,115 +14,109 @@ import {
   skills,
 } from "@/lib/constants";
 import { prefixHttps } from "@/lib/utils";
-import BeforePrint from "./_components/BeforePrint";
 
 export default function Page() {
   return (
-    <>
-      {/* Before Print */}
-      <BeforePrint />
+    <div className="py-16 print:py-0 space-y-6">
+      {/* Header */}
+      <section className="space-y-2">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-lg font-bold text-center">{details.name}</h1>
+          <p className="text-center">{details.title}</p>
+        </div>
 
-      <div className="py-12 print:py-0 space-y-6">
-        {/* Header */}
-        <header className="space-y-2">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-lg font-bold text-center">{details.name}</h1>
-            <p className="text-center">{details.title}</p>
+        <hr className="divide-y" />
+
+        <ul className="flex flex-wrap items-center justify-center md:justify-between print:justify-between gap-1 print:text-sm list-none list-inside">
+          <li>{details.location}</li>
+          <Dot size={16} />
+          <li>{details.email}</li>
+          <Dot size={16} />
+          <li>{details.phone}</li>
+          <Dot size={16} />
+          <li className="flex items-center gap-1">
+            <Construction size={12} /> {details.website}
+          </li>
+        </ul>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 print:grid-cols-2 place-items-center md:place-items-start print:place-items-start gap-2">
+          <div className="flex items-center gap-2">
+            <Linkedin size={14} stroke="0" className="fill-current" />
+            <Link href={prefixHttps(details.linkedin)} target="_blank">
+              {details.linkedin}
+            </Link>
           </div>
 
-          <hr className="divide-y" />
-
-          <ul className="flex flex-wrap items-center justify-center md:justify-between print:justify-between gap-1 print:text-sm list-none list-inside">
-            <li>{details.location}</li>
-            <Dot size={16} />
-            <li>{details.email}</li>
-            <Dot size={16} />
-            <li>{details.phone}</li>
-            <Dot size={16} />
-            <li className="flex items-center gap-1">
-              <Construction size={12} /> {details.website}
-            </li>
-          </ul>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 print:grid-cols-2 place-items-center md:place-items-start print:place-items-start gap-2">
-            <div className="flex items-center gap-2">
-              <Linkedin size={14} stroke="0" className="fill-current" />
-              <Link href={prefixHttps(details.linkedin)} target="_blank">
-                {details.linkedin}
-              </Link>
-            </div>
-
-            <div className="md:place-self-end print:place-self-end flex items-center gap-2">
-              <Github size={14} stroke="0" className="fill-current" />
-              <Link href={prefixHttps(details.github)} target="_blank">
-                {details.github}
-              </Link>
-            </div>
+          <div className="md:place-self-end print:place-self-end flex items-center gap-2">
+            <Github size={14} stroke="0" className="fill-current" />
+            <Link href={prefixHttps(details.github)} target="_blank">
+              {details.github}
+            </Link>
           </div>
-        </header>
+        </div>
+      </section>
 
-        <section id="objective" className="space-y-2 print:hidden">
-          <h2 className="font-bold text-center">Professional Summary</h2>
-          <p>{details.objective}</p>
-        </section>
+      <section id="objective" className="space-y-2 print:hidden">
+        <h2 className="font-bold text-center">Professional Summary</h2>
+        <p>{details.objective}</p>
+      </section>
 
-        <hr className="divide-y print:hidden" />
+      <hr className="divide-y print:hidden" />
 
-        {/* Education */}
-        <section id="education" className="space-y-2">
-          <h2 className="font-bold text-center">Education</h2>
+      {/* Education */}
+      <section id="education" className="space-y-2">
+        <h2 className="font-bold text-center">Education</h2>
 
-          <div className="space-y-4">
-            {education.map((item) => (
-              <EducationItem key={item.id} {...item} />
-            ))}
-          </div>
-        </section>
+        <div className="space-y-4">
+          {education.map((item) => (
+            <EducationItem key={item.id} {...item} />
+          ))}
+        </div>
+      </section>
 
-        {/* Experience */}
-        <section id="experience" className="space-y-2">
-          <h2 className="font-bold text-center">Relevant Experience</h2>
+      {/* Experience */}
+      <section id="experience" className="space-y-2">
+        <h2 className="font-bold text-center">Relevant Experience</h2>
 
-          <div className="space-y-4">
-            {experience.map((item) => (
-              <ExperienceItem key={item.id} {...item} />
-            ))}
-          </div>
-        </section>
+        <div className="space-y-4">
+          {experience.map((item) => (
+            <ExperienceItem key={item.id} {...item} />
+          ))}
+        </div>
+      </section>
 
-        {/* Technical Skills */}
-        <section id="technical-skills" className="space-y-2">
-          <h2 className="font-bold text-center">Technical Skills</h2>
+      {/* Technical Skills */}
+      <section id="technical-skills" className="space-y-2">
+        <h2 className="font-bold text-center">Technical Skills</h2>
 
-          <div className="space-y-2 md:space-y-1">
-            {skills.map((skill) => (
-              <SkillsItem key={skill.title} {...skill} />
-            ))}
-          </div>
-        </section>
+        <div className="space-y-2 md:space-y-1">
+          {skills.map((skill) => (
+            <SkillsItem key={skill.title} {...skill} />
+          ))}
+        </div>
+      </section>
 
-        {/* Projects */}
-        <section id="projects" className="space-y-2">
-          <h2 className="font-bold text-center">Projects</h2>
+      {/* Projects */}
+      <section id="projects" className="space-y-2">
+        <h2 className="font-bold text-center">Projects</h2>
 
-          <div className="space-y-2">
-            {projects.map((project) => (
-              <ProjectItem key={project.title} {...project} />
-            ))}
-          </div>
-        </section>
+        <div className="space-y-2">
+          {projects.map((project) => (
+            <ProjectItem key={project.title} {...project} />
+          ))}
+        </div>
+      </section>
 
-        {/* Certifications */}
-        <section id="certifications" className="space-y-2">
-          <h2 className="font-bold text-center">Certifications</h2>
+      {/* Certifications */}
+      <section id="certifications" className="space-y-2">
+        <h2 className="font-bold text-center">Certifications</h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 print:grid-cols-2 gap-2">
-            {certifications.map((certification) => (
-              <CertificationItem key={certification.title} {...certification} />
-            ))}
-          </div>
-        </section>
-      </div>
-    </>
+        <div className="grid grid-cols-1 md:grid-cols-2 print:grid-cols-2 gap-2">
+          {certifications.map((certification) => (
+            <CertificationItem key={certification.title} {...certification} />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
