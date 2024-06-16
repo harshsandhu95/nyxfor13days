529767bce9bd70f69c3b44bae628acf0a629ad7a
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-16 23:43:51 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -211,3 +211,10 @@ export const certifications: Certification[] = [
     skills: ["CSS", "Front-End Development", "HTML", "React.js", "JavaScript"],
   },
 ];
+
+export const navigation = [
+  { id: 0, name: "Home", link: "/" },
+  { id: 1, name: "Blog", link: "/blog" },
+  { id: 2, name: "Resume", link: "/cv" },
+  { id: 3, name: "Contact", link: "/contact" },
+];
