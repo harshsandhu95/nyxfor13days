bb386db0cc1064f97a1e3cadc57cc163521fffa6
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-16 16:36:22 +0530

    Update CV layout

--- src/app/cv/layout.tsx
+++ src/app/cv/layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 import { Source_Serif_4 } from "next/font/google";
-import { Toaster } from "@/components/ui/Sonner";
+import Header from "./_components/Header";
 import { cn } from "@/lib/utils";
 
 const font = Source_Serif_4({
@@ -10,9 +10,14 @@ const font = Source_Serif_4({
 
 export default function Provider({ children }: PropsWithChildren) {
   return (
-    <main className={cn(font.className, "container max-w-3xl print:text-sm")}>
-      {children}
-      <Toaster />
-    </main>
+    <div
+      className={cn(
+        font.className,
+        "relative container max-w-3xl print:text-sm",
+      )}
+    >
+      <Header />
+      <main>{children}</main>
+    </div>
   );
 }
