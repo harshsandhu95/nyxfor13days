8a3a378a5c5ed05cd6c861dcc7833848002e46a2
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-16 16:35:48 +0530

    Create Header.tsx

---
+++ src/app/cv/_components/Header.tsx
@@ -0,0 +1,31 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/Breadcrumb";
+import BeforePrint from "./BeforePrint";
+
+export default function Header() {
+  return (
+    <header className="py-2 px-4 absolute top-0 left-0 right-0 print:hidden flex items-center justify-between gap-2 z-20">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbSeparator />
+
+          <BreadcrumbItem>
+            <BreadcrumbPage>CV</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <BeforePrint />
+    </header>
+  );
+}
