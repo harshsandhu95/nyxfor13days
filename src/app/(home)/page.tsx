921117b12575b219b15abe3cab579d229c0db0ee
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-18 00:57:14 +0530

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,25 @@
+import Link from "next/link";
+import { HardHatIcon } from "lucide-react";
 import { Badge } from "@/components/ui/Badge";
 import { Button } from "@/components/ui/Button";
 import { details } from "@/lib/constants";
 
 export default function Home() {
   return (
-    <div className="min-h-[calc(100vh-64px)] py-12 space-y-12">
-      <section className="max-w-prose space-y-4">
-        <Badge>
-          The Website is under construction but feel free to view my cv.
+    <div className="min-h-[calc(100vh-64px)]">
+      <section className="py-12 max-w-prose space-y-4">
+        <Badge className="md:w-fit flex items-center justify-center gap-2">
+          <HardHatIcon size={16} /> Under Development
         </Badge>
         <h1 className="text-7xl eading-tight">Hey, I am Harsh Sandhu</h1>
         <p className="text-lg leading-normal">{details.objective}</p>
-        <Button>Let&apos;s Connect!</Button>
+        <Button asChild>
+          <Link href="">Let&apos;s Connect!</Link>
+        </Button>
+      </section>
+
+      <section className="py-12">
+        <h2 className="text-5xl">Education</h2>
       </section>
     </div>
   );
