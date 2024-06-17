9783068a802eee10360f065295c4885ab0c8b418
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-17 15:36:50 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -215,6 +215,6 @@ export const certifications: Certification[] = [
 export const navigation = [
   { id: 0, name: "Home", link: "/" },
   { id: 1, name: "Blog", link: "/blog" },
-  { id: 2, name: "Resume", link: "/cv" },
+  { id: 2, name: "Resume", link: "/resume" },
   { id: 3, name: "Contact", link: "/contact" },
 ];
