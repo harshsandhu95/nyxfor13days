f169467b5b06ac111d5015d812242f1ca891bd3c
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-18 23:34:56 +0530

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,30 +8,15 @@ export default async function Home() {
   const details = await getDetails();
 
   return (
-    <div className="min-h-[calc(100vh-64px)]">
-      <section className="py-12 max-w-prose space-y-4">
-        <Badge className="md:w-fit flex items-center justify-center gap-2">
-          <HardHatIcon size={16} /> Under Development
-        </Badge>
-        <h1 className="text-7xl eading-tight">Hey, I am {details?.name}</h1>
-        <p className="text-lg leading-normal">{details?.objective}</p>
-        <Button asChild>
-          <Link href="">Let&apos;s Connect!</Link>
-        </Button>
-      </section>
-
-      <section className="py-12 space-y-4">
-        <h2 className="text-5xl">Education</h2>
-
-        <p>
-          I possess a strong educational background in computer science, having
-          earned a B.Tech in Computer Science from CT University and a Diploma
-          in Computer Science from Guru Nanak Dev Engineering College. My
-          educational experiences have equipped me with expertise in various
-          areas, including Artificial Intelligence, System Design, Web
-          Development, and Cybersecurity.
-        </p>
-      </section>
-    </div>
+    <section className="py-12 max-w-prose space-y-4">
+      <Badge className="md:w-fit flex items-center justify-center gap-2">
+        <HardHatIcon size={16} /> Website Under Development
+      </Badge>
+      <h1 className="text-7xl eading-tight">Hey, I am {details?.name}</h1>
+      <p className="text-lg leading-normal">{details?.objective}</p>
+      <Button asChild>
+        <Link href="">Let&apos;s Connect!</Link>
+      </Button>
+    </section>
   );
 }
