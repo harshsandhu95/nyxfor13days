9407b51fa86e6f4dbeb3f48e03551a5c0c1141ad
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-18 10:31:24 +0530

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import { HardHatIcon } from "lucide-react";
 import { Badge } from "@/components/ui/Badge";
 import { Button } from "@/components/ui/Button";
-import { details } from "@/lib/constants";
+import { getDetails } from "../actions";
 
-export default function Home() {
+export default async function Home() {
+  const details = await getDetails();
+
   return (
     <div className="min-h-[calc(100vh-64px)]">
       <section className="py-12 max-w-prose space-y-4">
@@ -18,8 +20,17 @@ export default function Home() {
         </Button>
       </section>
 
-      <section className="py-12">
+      <section className="py-12 space-y-4">
         <h2 className="text-5xl">Education</h2>
+
+        <p>
+          I possess a strong educational background in computer science, having
+          earned a B.Tech in Computer Science from CT University and a Diploma
+          in Computer Science from Guru Nanak Dev Engineering College. My
+          educational experiences have equipped me with expertise in various
+          areas, including Artificial Intelligence, System Design, Web
+          Development, and Cybersecurity.
+        </p>
       </section>
     </div>
   );
