30fafdcd463788c294f31362610e7b97eab94eb7
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-19 16:55:42 +0530

    Update fonts.ts

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,8 +1,8 @@
-import { Inter, Source_Serif_4 } from "next/font/google";
+import { Inter, Libre_Baskerville } from "next/font/google";
 
 const sansFont = Inter({ subsets: ["latin"], variable: "--font-sans" });
-const serifFont = Source_Serif_4({
-  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+const serifFont = Libre_Baskerville({
+  weight: ["400", "700"],
   subsets: ["latin"],
   variable: "--font-serif",
 });
