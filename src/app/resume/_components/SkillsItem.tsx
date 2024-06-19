7f83d17556f51b81f04eea9f2befdc80ec991ee4
Harsh Sandhu <nyxfor13days@gmail.com>
2024-06-19 16:58:56 +0530

    Update SkillsItem.tsx

--- src/app/resume/_components/SkillsItem.tsx
+++ src/app/resume/_components/SkillsItem.tsx
@@ -2,8 +2,8 @@ import { skills } from "@prisma/client";
 
 interface Props extends skills {}
 
-export default function SkillsItem({ title, skills }: Props) {
-  const skillsString = skills.join(", ");
+export default function SkillsItem({ title, items }: Props) {
+  const skillsString = items.map((item) => item.name).join(", ");
 
   return (
     <div className="flex flex-col md:flex-row print:flex-row gap-1 md:gap-2 print:gap-2">
